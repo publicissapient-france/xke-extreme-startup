d5e9a69c930b84387edb9161b0ec04fded1266f5
Jean Helou <jean.helou@gmail.com>
2012-09-02 19:49:52 +0200

    Closes #5 what is the http response code of a given url
    L'implem est pas hyper clean mais ça marche (testé vs un serveur client :/)

--- server/lib/extreme_startup/question_factory.rb
+++ server/lib/extreme_startup/question_factory.rb
@@ -485,6 +485,31 @@ module ExtremeStartup
     end
   end
 
+  class HttpResponseQuestion < Question
+    RESPONSE_CODES=[200,201,202,203,204,205,206,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,
+                    500,501,502,503,504,505]
+    REGISTERED_RESPONSES=Hash.new
+    def initialize(player, uuid=nil,responsecode=nil)
+      baseurl=ENV['BASEURL'] || "http://localhost:3000/"
+      @url=baseurl+"http_code?uid="
+      @id=uuid
+      if(responsecode)
+        @code=responsecode
+      else
+        @code=RESPONSE_CODES.sample
+      end
+    end
+    def as_text
+      REGISTERED_RESPONSES[self.id]=@code
+      "what is the http response code of #{@url+id.to_s}"
+    end
+    private
+    def correct_answer
+      REGISTERED_RESPONSES.delete(self.id)
+      @code
+    end
+  end
+
   class QuestionFactory
     attr_reader :round
 
